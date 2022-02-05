14fec84b3ec0cbf0747db74888eb52f0b9a0d88a
Arinjay <2005441@kiit.ac.in>
2022-02-05 16:29:26 +0530

    vscode installation

--- setup.sh
+++ setup.sh
@@ -35,4 +35,14 @@ else
     echo "$rpass" | chpasswd
     echo "User created successfully"
 fi
+sudo -i
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y git wget curl build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake cmake libuv1-dev libmicrohttpd-dev libssl-dev jq libsodium-dev zlib1g-dev software-properties-common apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt install code
+sudo apt autoremove -y
 
